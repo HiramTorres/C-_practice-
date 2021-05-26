abe38305708201154889b59cad2019d8bc72885f
Hiram Torres <hiram.ht10@gmail.com>
2021-05-25 21:08:44 -0500

    Primer commit

---
+++ Clase.c
@@ -0,0 +1,43 @@
+bit boolean = 1; // 1 bit  
+chart character = 'A' ; // 8 bits en ASCII  
+short entero_8b = 225; // 8 bits de -127 a 127   
+int entero_16b = 123; // 16 bits -31767 a 32767
+long 
+float 
+double 
+
+
+int main()
+{
+    int valor1 = 0;
+    char character1 = 'z';
+    int array [5] = {1,2,3,4,5}; // indice n-1
+    int array1 [2][2] = {{1,2}{3,4}}
+    short array2 [2][2][2] = {
+                            {{1,2},{3,4}},{{1,2},{3,4}}
+
+                             }
+}
+
+typedef struct
+{
+    char Nombre[10];
+    int Edad;
+
+}Usuario  
+
+int main ()
+{
+    Usuario U;
+    Usuario A;
+
+    U.Nombre = "Predro"
+    A.Nombre = "Alicia";
+    A.Edad = 36;
+    U.Edad = 17;
+    printf("%d",A.Edad);
+    
+    return 0;
+    }
+    
+
